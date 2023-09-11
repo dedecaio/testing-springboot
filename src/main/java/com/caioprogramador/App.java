9c3d5fc8b77d80db37361a681bac490d2468b909
Caio <caio.florees2005@gmail.com>
2023-09-11 15:42:12 -0300

    Initial commit

---
+++ src/main/java/com/caioprogramador/App.java
@@ -0,0 +1,13 @@
+package com.caioprogramador;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/caioprogramador/FooBarTest.java
@@ -0,0 +1,2 @@
+package com.caioprogramador;public class FooBarTest {
+}
